cc92e06042ee9aa9f5c934172ad18a9098e98b2d
Caroline Meira <caroline.meira987@gmail.com>
2022-12-22 02:59:37 -0300

    Insere projeto

---
+++ src/pages/Inicio.jsx
@@ -0,0 +1,10 @@
+import { Header } from '../components/Header'
+import homeImg from '../assets/home.png'
+
+export function Inicio() {
+    return (
+       <>
+        <Header title="Meu site lindo" image={homeImg} />
+       </>
+    )
+}
